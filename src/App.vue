4b7afa323fc9e860ae53efbaec1fe4253a5dfc44
joey <joey@anttoweb.com>
2022-06-13 17:23:48 +0800

    2022-6-13

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <router-link to="/home">首页</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ElMessage } from "element-plus"
+
+  const showElmessage = ()=>{
+    ElMessage.success('Hello')
+  }
+</script>
+
+<template>
+  <div>
+    <el-button @click="showElmessage">显示elmessage</el-button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
